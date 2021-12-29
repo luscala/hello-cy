fba8451f1155dd25c88132ec54793da07f9b9a10
Luca Scala <lucascala05@gmail.com>
2021-12-29 09:39:41 +0100

    Setting up TypeScript

---
+++ cypress/integration/3-hello-cy/hello-cy.spec.ts
@@ -0,0 +1,23 @@
+describe('hello-cy tests', () => {
+  beforeEach(() => {
+    cy.visit('https://example.cypress.io/todo');
+  });
+
+  it('displays two todo items by default', () => {
+    cy.get('.todo-list li').should('have.length', 2);
+
+    cy.get('.todo-list li').first().should('have.text', 'Pay electric bill');
+    cy.get('.todo-list li').last().should('have.text', 'Walk the dog');
+  });
+
+  it('can add new todo items', () => {
+    const newItem = 'Feed the cat';
+
+    cy.get('[data-test=new-todo]').type(`${newItem}{enter}`);
+
+    cy.get('.todo-list li')
+      .should('have.length', 3)
+      .last()
+      .should('have.text', newItem);
+  });
+});
